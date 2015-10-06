6146a675ba988c2b34353f91551c7db6c8c70f2e
linwei365 <linwei365@gmail.com>
2015-10-06 13:45:26 -0700

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Lin Wei on 10/6/15.
+//  Copyright © 2015 Lin Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
